97f379dacf4cb88010ba710bdd7bab7862727549
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-28 17:09:12 -0600

    Missed file from rock paper scissors kata

--- 02-RockPaperScissors/java/src/main/java/PaperGame.java
+++ 02-RockPaperScissors/java/src/main/java/PaperGame.java
@@ -2,7 +2,7 @@ public class PaperGame implements Game {
   public String getResult(String gesture) {
     if (gesture.equals("paper"))
       return "scratch";
-    
+
     if (gesture.equals("rock"))
       return "paper beats rock";
 
