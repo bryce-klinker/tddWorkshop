d026461031109777c9c6938a1df5a89339fe2687
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-28 15:07:11 -0600

    Refactored field initialization into setup method

--- 01-FizzBuzz/java/src/test/java/FizzBuzzTest.java
+++ 01-FizzBuzz/java/src/test/java/FizzBuzzTest.java
@@ -1,27 +1,32 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
-	@Test
+    private FizzBuzz _fizzBuzz;
+
+    @Before
+    public void setUp() {
+        _fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
     public void shouldReturn1For1() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-        String actualReturnValue = fizzBuzz.process(1);
+        String actualReturnValue = _fizzBuzz.process(1);
         assertEquals("1", actualReturnValue);
     }
 
     @Test
     public void shouldReturn2For2() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actualReturnValue = fizzBuzz.process(2);
+        String actualReturnValue = _fizzBuzz.process(2);
         assertEquals("2", actualReturnValue);
     }
 
     @Test
     public void shouldReturnFizzFor3() {
-	    FizzBuzz fizzBuzz = new FizzBuzz();
-	    String actual = fizzBuzz.process(3);
+	    String actual = _fizzBuzz.process(3);
 	    assertEquals("Fizz", actual);
     }
 }
