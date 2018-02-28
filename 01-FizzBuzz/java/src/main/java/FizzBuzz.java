629745e7c5e17334b8e016aa061566d5fdd27689
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-28 15:14:10 -0600

    Core FizzBuzz logic implemented

--- 01-FizzBuzz/java/src/main/java/FizzBuzz.java
+++ 01-FizzBuzz/java/src/main/java/FizzBuzz.java
@@ -1,8 +1,14 @@
 public class FizzBuzz {
 
     public String process(Integer inputValue) {
-        if (inputValue == 3)
+        if (inputValue % 3 == 0 && inputValue % 5 == 0)
+            return "FizzBuzz";
+
+        if (inputValue % 3 == 0)
             return "Fizz";
+
+        if (inputValue % 5 == 0)
+            return "Buzz";
         return inputValue.toString();
     }
 }
--- 01-FizzBuzz/java/src/test/java/FizzBuzzTest.java
+++ 01-FizzBuzz/java/src/test/java/FizzBuzzTest.java
@@ -14,19 +14,49 @@ public class FizzBuzzTest {
 
     @Test
     public void shouldReturn1For1() {
-        String actualReturnValue = _fizzBuzz.process(1);
-        assertEquals("1", actualReturnValue);
+        String actual = _fizzBuzz.process(1);
+        assertEquals("1", actual);
     }
 
     @Test
     public void shouldReturn2For2() {
-        String actualReturnValue = _fizzBuzz.process(2);
-        assertEquals("2", actualReturnValue);
+        String actual = _fizzBuzz.process(2);
+        assertEquals("2", actual);
     }
 
     @Test
     public void shouldReturnFizzFor3() {
 	    String actual = _fizzBuzz.process(3);
 	    assertEquals("Fizz", actual);
+    }
+
+    @Test
+    public void shouldReturnBuzzFor5() {
+        String actual = _fizzBuzz.process(5);
+        assertEquals("Buzz", actual);
+    }
+
+    @Test
+    public void shouldReturnFizzForValueDivisibleByThree() {
+        String actual = _fizzBuzz.process(6);
+        assertEquals("Fizz", actual);
+    }
+
+    @Test
+    public void shouldReturnBuzzForValueDivisibleByFive() {
+        String actual = _fizzBuzz.process(10);
+        assertEquals("Buzz", actual);
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzFor15() {
+        String actual = _fizzBuzz.process(15);
+        assertEquals("FizzBuzz", actual);
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzForValueDivisibleByThreeAndFive() {
+        String actual = _fizzBuzz.process(30);
+        assertEquals("FizzBuzz", actual);
     }
 }
