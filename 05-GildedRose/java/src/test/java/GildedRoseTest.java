5f4c2058d4bbb9d5a76f0400a7f6b7cf130dd039
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-06 14:26:00 -0700

    Updated updated test to ensure backstage pass increases in quality

--- 05-GildedRose/java/src/test/java/GildedRoseTest.java
+++ 05-GildedRose/java/src/test/java/GildedRoseTest.java
@@ -9,12 +9,12 @@ public class GildedRoseTest {
   public void shouldDecreaseQualityOfItems() {
     GildedRose.main(null);
 
-    assertItemEquals("+5 Dexterity Vest", 19, 9, GildedRose.items.get(0));
+    assertItemEquals("+5 Dexterity Vest", 9, 19, GildedRose.items.get(0));
     assertItemEquals("Aged Brie", 1, 1, GildedRose.items.get(1));
-    assertItemEquals("Elixir of the Mongoose", 6, 4, GildedRose.items.get(2));
-    assertItemEquals("Sulfuras, Hand of Ragnaros", 80, 0, GildedRose.items.get(3));
-    assertItemEquals("Backstage passes to a TAFKAL80ETC concert", 21, 14, GildedRose.items.get(4));
-    assertItemEquals("Conjured Mana Cake", 5, 2, GildedRose.items.get(5));
+    assertItemEquals("Elixir of the Mongoose", 4, 6, GildedRose.items.get(2));
+    assertItemEquals("Sulfuras, Hand of Ragnaros", 0, 80, GildedRose.items.get(3));
+    assertItemEquals("Backstage passes to a TAFKAL80ETC concert", 14, 21, GildedRose.items.get(4));
+    assertItemEquals("Conjured Mana Cake", 2, 5, GildedRose.items.get(5));
   }
 
   @Test
@@ -22,14 +22,22 @@ public class GildedRoseTest {
     GildedRose.main(null);
     GildedRose.updateQuality();
 
-    assertItemEquals("+5 Dexterity Vest", 18, 8, GildedRose.items.get(0));
-    assertItemEquals("Aged Brie", 2, 0, GildedRose.items.get(1));
-    assertItemEquals("Elixir of the Mongoose", 5, 3, GildedRose.items.get(2));
-    assertItemEquals("Sulfuras, Hand of Ragnaros", 80, 0, GildedRose.items.get(3));
-    assertItemEquals("Backstage passes to a TAFKAL80ETC concert", 22, 13, GildedRose.items.get(4));
+    assertItemEquals("+5 Dexterity Vest", 8, 18, GildedRose.items.get(0));
+    assertItemEquals("Aged Brie", 0, 2, GildedRose.items.get(1));
+    assertItemEquals("Elixir of the Mongoose", 3, 5, GildedRose.items.get(2));
+    assertItemEquals("Sulfuras, Hand of Ragnaros", 0, 80, GildedRose.items.get(3));
+    assertItemEquals("Backstage passes to a TAFKAL80ETC concert", 13, 22, GildedRose.items.get(4));
   }
 
-  private void assertItemEquals(String expectedName, int expectedQuality, int expectedSellIn, Item actualItem) {
+  @Test
+  public void shouldIncreaseQualityTwoForBackstageItemWhenSellInLessThanTen() {
+    String name = "Backstage passes to a TAFKAL80ETC concert";
+    Item item = new Item(name, 10, 12);
+    GildedRose.updateItem(item);
+    assertItemEquals(name, 9, 14, item);
+  }
+
+  private void assertItemEquals(String expectedName, int expectedSellIn, int expectedQuality, Item actualItem) {
     assertEquals(expectedName, actualItem.name);
     assertEquals(expectedQuality, actualItem.quality);
     assertEquals(expectedSellIn, actualItem.sellIn);
