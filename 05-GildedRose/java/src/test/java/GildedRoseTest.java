ca0f78219ed7cc3d273d5bf539e67d8e5cf153ee
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-06 13:50:24 -0700

    Created smalle helper to assert item properties match

--- 05-GildedRose/java/src/test/java/GildedRoseTest.java
+++ 05-GildedRose/java/src/test/java/GildedRoseTest.java
@@ -8,12 +8,16 @@ public class GildedRoseTest {
   @Test
   public void shouldDecreaseQualityOfItems() {
     GildedRose.main(null);
-    assertEquals("+5 Dexterity Vest", GildedRose.items.get(0).name);
-    assertEquals(19, GildedRose.items.get(0).quality);
-    assertEquals(9, GildedRose.items.get(0).sellIn);
+    assertItemEquals("+5 Dexterity Vest", 19, 9, GildedRose.items.get(0));
 		for (int i = 0; i < GildedRose.items.size(); ++i) {
 		  Item item = GildedRose.items.get(i);
 		  System.out.println("Item: " + item.name +  " quality: " + item.quality + " sellin: " + item.sellIn);
     }
+  }
+
+  private void assertItemEquals(String expectedName, int expectedQuality, int expectedSellIn, Item actualItem) {
+    assertEquals(expectedName, actualItem.name);
+    assertEquals(expectedQuality, actualItem.quality);
+    assertEquals(expectedSellIn, actualItem.sellIn);
   }
 }
