f4189f7dba25a0e1ae678357108fa7b2c2b3483a
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-06 13:59:56 -0700

    Added test to verify behavior after two days

--- 05-GildedRose/java/src/test/java/GildedRoseTest.java
+++ 05-GildedRose/java/src/test/java/GildedRoseTest.java
@@ -8,12 +8,25 @@ public class GildedRoseTest {
   @Test
   public void shouldDecreaseQualityOfItems() {
     GildedRose.main(null);
+
     assertItemEquals("+5 Dexterity Vest", 19, 9, GildedRose.items.get(0));
     assertItemEquals("Aged Brie", 1, 1, GildedRose.items.get(1));
     assertItemEquals("Elixir of the Mongoose", 6, 4, GildedRose.items.get(2));
     assertItemEquals("Sulfuras, Hand of Ragnaros", 80, 0, GildedRose.items.get(3));
     assertItemEquals("Backstage passes to a TAFKAL80ETC concert", 21, 14, GildedRose.items.get(4));
     assertItemEquals("Conjured Mana Cake", 5, 2, GildedRose.items.get(5));
+  }
+
+  @Test
+  public void shouldContinueToDecreaseQualityOfItemsAfterTwoDays() {
+    GildedRose.main(null);
+    GildedRose.updateQuality();
+
+    assertItemEquals("+5 Dexterity Vest", 18, 8, GildedRose.items.get(0));
+    assertItemEquals("Aged Brie", 2, 0, GildedRose.items.get(1));
+    assertItemEquals("Elixir of the Mongoose", 5, 3, GildedRose.items.get(2));
+    assertItemEquals("Sulfuras, Hand of Ragnaros", 80, 0, GildedRose.items.get(3));
+    assertItemEquals("Backstage passes to a TAFKAL80ETC concert", 22, 13, GildedRose.items.get(4));
   }
 
   private void assertItemEquals(String expectedName, int expectedQuality, int expectedSellIn, Item actualItem) {
