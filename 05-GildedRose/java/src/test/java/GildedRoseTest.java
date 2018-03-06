e89b2868e4d5419e56c4229efeb748ed90fb1433
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-06 13:52:10 -0700

    Filling in asserts for items

--- 05-GildedRose/java/src/test/java/GildedRoseTest.java
+++ 05-GildedRose/java/src/test/java/GildedRoseTest.java
@@ -9,6 +9,9 @@ public class GildedRoseTest {
   public void shouldDecreaseQualityOfItems() {
     GildedRose.main(null);
     assertItemEquals("+5 Dexterity Vest", 19, 9, GildedRose.items.get(0));
+    assertItemEquals("Aged Brie", 1, 1, GildedRose.items.get(1));
+    assertItemEquals("Elixir of the Mongoose", 6, 4, GildedRose.items.get(2));
+    assertItemEquals("Sulfuras, Hand of Ragnaros", 80, 0, GildedRose.items.get(3));
 		for (int i = 0; i < GildedRose.items.size(); ++i) {
 		  Item item = GildedRose.items.get(i);
 		  System.out.println("Item: " + item.name +  " quality: " + item.quality + " sellin: " + item.sellIn);
