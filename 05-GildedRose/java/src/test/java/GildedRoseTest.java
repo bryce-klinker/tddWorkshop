82af6b655def0c7baaa03b12b06b6a40faa8064e
Bryce Klinker <bklinker@pillartechnology.com>
2018-03-06 13:48:15 -0700

    Added assert for first item state after one run

--- 05-GildedRose/java/src/test/java/GildedRoseTest.java
+++ 05-GildedRose/java/src/test/java/GildedRoseTest.java
@@ -1,11 +1,16 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 
 public class GildedRoseTest {
 
   @Test
   public void shouldDecreaseQualityOfItems() {
     GildedRose.main(null);
+    assertEquals("+5 Dexterity Vest", GildedRose.items.get(0).name);
+    assertEquals(19, GildedRose.items.get(0).quality);
+    assertEquals(9, GildedRose.items.get(0).sellIn);
 		for (int i = 0; i < GildedRose.items.size(); ++i) {
 		  Item item = GildedRose.items.get(i);
 		  System.out.println("Item: " + item.name +  " quality: " + item.quality + " sellin: " + item.sellIn);
